5283075438dacf96dd77eee6cf4a40a073d93c3c
Cristian Toaquiza <crissmtl@gmail.com>
2017-03-02 22:22:33 -0500

    configurar tareas para construir y poner en producción la app

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,6 +9,12 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var inject = require('gulp-inject');
 var wiredep = require('wiredep').stream;
+var templateCache = require('gulp-angular-templatecache');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var uncss = require('gulp-uncss');
 
 gulp.task('server', function () {
   connect.server({
@@ -64,3 +70,51 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+
+gulp.task('templates', function () {
+  gulp.src('./app/views/**/*.tpl.html')
+    .pipe(templateCache({
+      root: 'views/',
+      module: 'blog.templates',
+      standalone: true
+    }))
+    .pipe(gulp.dest('./app/scripts'));
+});
+
+gulp.task('compress', function () {
+  gulp.src('./app/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify({mangle: false})))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy', function () {
+  gulp.src('./app/index.html')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist'));
+  gulp.src('./app/lib/font-awesome/fonts/**')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('uncss', function () {
+  gulp.src('./dist/css/style.min.css')
+    .pipe(uncss({
+      html: ['./app/index.html', './app/views/post-detail.tpl.html', './app/views/post-list.tpl.html']
+    }))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build', ['templates', 'compress', 'copy', 'uncss']);
+
+gulp.task('server-dist', function () {
+  connect.server({
+    root: './dist',
+    hostname: '0.0.0.0',
+    port: 8080,
+    livereload: true,
+    middleware: function (connect, opt) {
+      return [historyApiFallback()];
+    }
+  });
+});
